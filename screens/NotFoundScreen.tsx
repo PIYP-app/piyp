5d8805d4b56ed9b44c46f51dd2fdf368af6d7cae
Alex <amoinier@outlook.fr>
2022-03-13 16:52:46 +0000

    chore: add eslint config + format files

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -1,9 +1,30 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native'
 
-import { Text, View } from '../components/Themed';
-import { RootStackScreenProps } from '../types';
+import { Text, View } from '../components/Themed'
+import { RootStackScreenProps } from '../types'
 
-export default function NotFoundScreen({ navigation }: RootStackScreenProps<'NotFound'>) {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15
+  },
+  linkText: {
+    fontSize: 14,
+    color: '#2e78b7'
+  }
+})
+
+export default function NotFoundScreen ({ navigation }: RootStackScreenProps<'NotFound'>) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>This screen doesn't exist.</Text>
@@ -11,26 +32,5 @@ export default function NotFoundScreen({ navigation }: RootStackScreenProps<'Not
         <Text style={styles.linkText}>Go to home screen!</Text>
       </TouchableOpacity>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
--- types.tsx
+++ types.tsx
@@ -3,15 +3,9 @@
  * https://reactnavigation.org/docs/typescript/
  */
 
-import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
-import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-
-declare global {
-  namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
-  }
-}
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
+import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
 export type RootStackParamList = {
   Root: NavigatorScreenParams<RootTabParamList> | undefined;
