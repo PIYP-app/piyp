b2dd57cbc8b537fb69e199ad21ef3a48ae404333
Alex Moinier <amoinier@outlook.fr>
2024-07-23 15:13:47 +0200

    fix: desktop OK

--- lib/thumbnail.dart
+++ lib/thumbnail.dart
@@ -1,21 +1,26 @@
 import 'dart:io';
-import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:video_thumbnail/video_thumbnail.dart';
 import 'package:webdav_client/webdav_client.dart' as webdav;
+import 'package:flutter/services.dart' show ByteData, rootBundle;
 
 class Thumbnail {
   static initThumbnailFolder() async {
-    final directory = await getApplicationDocumentsDirectory();
-    final thumbnailDirectory = Directory('${directory.path}/thumbnails');
-    if (!await thumbnailDirectory.exists()) {
-      await thumbnailDirectory.create();
-    }
+    try {
+      final directory = await getApplicationDocumentsDirectory();
+      final thumbnailDirectory = Directory('${directory.path}/thumbnails');
+      if (!await thumbnailDirectory.exists()) {
+        await thumbnailDirectory.create();
+      }
 
-    print('thumbnail folder created at: ${directory.path}/thumbnails');
+      print('thumbnail folder created at: ${directory.path}/thumbnails');
+    } catch (e) {
+      print(e);
+    }
   }
 
   static Future<File> generateFilePath(String eTag) async {
@@ -44,28 +49,38 @@ class Thumbnail {
 
   static Future<Uint8List?> generateVideoThumbnail(
       String url, String eTag) async {
-    final preferences = await SharedPreferences.getInstance();
+    try {
+      final preferences = await SharedPreferences.getInstance();
 
-    final fileBytes = await VideoThumbnail.thumbnailData(
-      video: '${preferences.getString('webdav_uri')}/$url',
-      headers: {
-        'Authorization': webdav.BasicAuth(
-                user: preferences.getString('webdav_user') ?? '',
-                pwd: preferences.getString('webdav_password') ?? '')
-            .authorize('', '')
-      },
-      imageFormat: ImageFormat.JPEG,
-      maxHeight: 500,
-      quality: 1,
-    );
+      final fileBytes = await VideoThumbnail.thumbnailData(
+        video: '${preferences.getString('webdav_uri')}/$url',
+        headers: {
+          'Authorization': webdav.BasicAuth(
+                  user: preferences.getString('webdav_user') ?? '',
+                  pwd: preferences.getString('webdav_password') ?? '')
+              .authorize('', '')
+        },
+        imageFormat: ImageFormat.JPEG,
+        maxHeight: 500,
+        quality: 1,
+      );
 
-    if (fileBytes == null) {
-      return null;
+      if (fileBytes == null) {
+        return null;
+      }
+
+      await saveThumbnail(eTag, fileBytes);
+
+      return fileBytes;
+    } catch (error) {
+      ByteData data = await rootBundle.load("assets/video.png");
+      var fileBytes =
+          data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+      await saveThumbnail(eTag, fileBytes);
+
+      return fileBytes;
     }
-
-    await saveThumbnail(eTag, fileBytes);
-
-    return fileBytes;
   }
 
   static Future<Uint8List?> readThumbnail(String eTag) async {
